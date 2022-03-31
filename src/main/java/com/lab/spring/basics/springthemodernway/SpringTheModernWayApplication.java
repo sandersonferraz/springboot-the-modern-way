82eb7e95832a812dedca4a79e300d1dfa24c5ce1
Sanderson Ferraz <sandersonferraz@gmail.com>
2022-03-30 23:54:50 -0300

    Fisrt commit

---
+++ src/main/java/com/lab/spring/basics/springthemodernway/SpringTheModernWayApplication.java
@@ -0,0 +1,13 @@
+package com.lab.spring.basics.springthemodernway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTheModernWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTheModernWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/spring/basics/springthemodernway/SpringTheModernWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.spring.basics.springthemodernway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTheModernWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
